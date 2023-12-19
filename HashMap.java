d123de57f134d79e11b8b0c88eda5bfe0c916123
jagadeeshchandranaidu <70916273+jagadeeshchandranaidu@users.noreply.github.com>
2023-12-19 10:19:43 +0530

    Create HashMap.java

---
+++ HashMap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class HashMap1 {
+    public static void main(String[] args) {
+        
+        HashMap<String, Integer> employees = new HashMap<>();
+
+         
+       employees.put("Jhon", 1);
+        employees.put("Tyrion", 2);
+         employees.put("Tywin", 3);
+
+         
+           System.out.println( employees.get("ram"));
+
+         employees.remove("Tywin");
+
+        
+    }
+}
